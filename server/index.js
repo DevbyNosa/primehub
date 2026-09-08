44c820d54c4807549bad2f5f321b0bc1b7855c07
Judges Nosakhare Igbinosa <judgesnigbinosa@gmail.com>
2026-09-08 19:57:08 +0100

    re update cors config

--- server/index.js
+++ server/index.js
@@ -46,7 +46,8 @@ app.use(cors({
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Set-Cookie']
 }));
 
 // Logging
