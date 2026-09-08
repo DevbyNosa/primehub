b92855178ba6dbb381dae5ec6b04a55d195f5a28
Judges Nosakhare Igbinosa <judgesnigbinosa@gmail.com>
2026-09-08 20:20:53 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -60,7 +60,7 @@ app.use(compression());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-app.set('trust proxy', 1);
+
 app.use(sessionConfig);
 //app.use(checkBanStatus)
 
